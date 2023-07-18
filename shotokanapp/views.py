dd10929da91996011c94079c147b06fffe653877
Paul-Gleeson <Shagamatula@gmail.com>
2023-07-18 15:40:22 +0000

    inital commit

--- shotokanapp/views.py
+++ shotokanapp/views.py
@@ -1,3 +1,9 @@
+from django.views.generic import View
 from django.shortcuts import render
 
 # Create your views here.
+
+class firstpage(View):
+    def get(self, request):
+        context = {}
+        return render(request, 'index.html', context)
