4b47a2acbacc0fc43702292b7d57fa3df5c4b607
Paul-Gleeson <Shagamatula@gmail.com>
2023-07-20 20:16:47 +0000

    add edit code and page edit_record

--- shotokanapp/views.py
+++ shotokanapp/views.py
@@ -1,13 +1,14 @@
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import StudentInfoMod
 from .forms import StudentForm, KyuRegisterForm, UpdateStudentForm
 from django.views.generic.edit import FormView, UpdateView
 from django.urls import reverse, reverse_lazy
 from django.views.generic.base import TemplateView
-
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+
 
 class firstpage(View):
     def get(self, request, *args, **kwargs):
@@ -54,16 +55,92 @@ class StudentListView(TemplateView):
         context['student'] = StudentInfoMod.objects.all()
         return context
 
+
 class UpdateStudentView(UpdateView):
     model = StudentInfoMod
-    template_name = 'edit_record.html'
     form_class = UpdateStudentForm
+    template_name = 'edit_record.html'
     success_url = reverse_lazy('success')
 
-    # def get_object(self, queryset=None):
-    #     obj = get_object_or_404(self.model, Student_ID=self.kwargs['Student_ID'])
-    #     return obj
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pk'] = self.kwargs['pk']
+        return context
 
-    # def form_valid(self, form):
-    #     form.save()
-    #     return super().form_valid(form)
+    def get_success_url(self):
+        return reverse('edit_record', kwargs={'pk': self.object.pk})
+
+    # def get_object(self, queryset=None):
+    #     id_ = self.kwargs.get('id')
+    #     try:
+    #         return get_object_or_404(StudentInfoMod, pk=id_)
+    #     except StudentInfoMod.DoesNotExist:
+    #         raise Http404('Does Not Exist')
+
+
+# class UpdateStudentView(UpdateView):
+#     model = StudentInfoMod
+#     template_name = 'edit_record.html'
+#     form_class = UpdateStudentForm
+#     success_url = reverse_lazy('success')
+
+#     def get_object(self, queryset=None):
+#         id_ = self.kwargs.get('id')
+#         try:
+#             return get_object_or_404(StudentInfoMod, pk=id_)
+#         except StudentInfoMod.DoesNotExist:
+#             raise Http404('Does Not Exist')
+
+# class UpdateStudentView(UpdateView):
+#     model = StudentInfoMod
+#     template_name = 'edit_record.html'
+#     form_class = UpdateStudentForm
+#     success_url = reverse_lazy('success')
+
+#     def get_object(self, queryset=None):
+#         id_ = self.kwargs.get('id')
+#         try:
+#             return get_object_or_404(StudentInfoMod, id=id_)
+#         except StudentInfoMod.DoesNotExist:
+#             raise Http404('Does Not Exist')
+
+#     # def get_context_data(self, **kwargs):
+#     #     context = super().get_context_data(**kwargs)
+#     #     obj = self.get_object()
+#     #     context['student_id'] = obj
+#     #     return context
+
+
+# class UpdateStudentView(UpdateView):
+#     model = StudentInfoMod
+#     template_name = 'edit_record.html'
+#     form_class = UpdateStudentForm
+#     success_url = reverse_lazy('success')
+
+#     def get_object(self, queryset=None):
+#         id_ = self.kwargs.get('id')
+#         return get_object_or_404(StudentInfoMod, id=id_)
+
+#     def form_valid(self, form):
+#         self.object = form.save()
+#         return super().form_valid(form)
+
+
+# class UpdateStudentView(UpdateView):
+#     model = StudentInfoMod
+#     template_name = 'edit_record.html'
+#     form_class = UpdateStudentForm
+#     success_url = reverse_lazy('success')
+
+#     def get_object(self, queryset=None):
+#         id_ = self.kwargs.get('id')
+#         try:
+#             return get_object_or_404(StudentInfoMod, pk=id_)
+#         except StudentInfoMod.DoesNotExist:
+#             raise Http404('Does Not Exist')
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         obj = self.get_object()
+#         context['student_id'] = obj
+#         return context
